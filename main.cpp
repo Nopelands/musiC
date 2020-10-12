857ceb0239d9ee1d3aa628c3cff7372ac79c686b
Nopelands <rhas@cin.ufpe.br>
2020-10-12 19:50:48 -0300

    Working main loop

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 #include <pthread.h>
 #include <ncurses.h>
+#include <string>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    bool exit = false;
+    string key;
+    while (!exit) {
+        std::cout << "Welcome to musiC++!" << std::endl;
+        std::cout << "press q to quit" << std::endl;
+        std::cin >> key;
+        if (key == "q") {
+            exit = true;
+        }
+    }
     return 0;
 }
